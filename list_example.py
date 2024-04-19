fdd09d1175be8a7c9f3f56beaebe74064475950c
Curtis1992 <155851972+Curtis1992@users.noreply.github.com>
2024-04-19 11:42:08 -0500

    Add files via upload

---
+++ list_example.py
@@ -0,0 +1,14 @@
+amazon_cart = [
+  'notebooks',
+  'sunglasses',
+  'comicbooks',
+  'pencils'
+]
+
+
+
+amazon_cart[0] = 'laptop'
+new_cart = amazon_cart[:]
+new_cart[0] = 'tv'
+print(new_cart)
+print(amazon_cart)
